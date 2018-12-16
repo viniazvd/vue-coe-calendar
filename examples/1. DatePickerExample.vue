1fca3637c23684d9a9cf565cf6ca0de571c3b56a
Vitor Luiz Cavalcanti <vitorluizc@outlook.com>
2018-12-16 03:36:11 -0200

    :building_construction: move examples and library usage to examples/ and use poi instead

---
+++ examples/1. DatePickerExample.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="DatePicker">
+    <input
+      class="DatePicker__input"
+      type="text"
+      v-model="date"
+      @blur="isFocused = false"
+      @focus="isFocused = true"
+    >
+
+    <calendar
+      is-range
+      :date="date"
+      :show="isFocused"
+      @show="(status) => isFocused = status"
+      @date-handler="(value) => date = value"
+    />
+  </div>
+</template>
+
+<script>
+import Calendar from '../src/Index.vue'
+
+export default {
+  components: { Calendar },
+  data () {
+    return {
+      date: '',
+      isFocused: false
+    }
+  }
+}
+</script>
