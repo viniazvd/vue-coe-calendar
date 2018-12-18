1b4bb9ac69c1c94177b3929afd27294881f50d92
Vinicius Azevedo <vinicius.azevedo@convenia.com.br>
2018-12-18 16:10:31 -0200

    add global directive

--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// directive
+import outside from './outside.js'
+
 Vue.config.productionTip = false
+
+Vue.directive('outside', outside)
 
 new Vue({
   render: h => h(App)
